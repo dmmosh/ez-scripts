50269078dc2b724da1dbff37254efc7744bdd30a
wettestsock <evilsharkeater@gmail.com>
2024-01-27 22:12:51 -0500

    hello guys

--- git-all.sh
+++ git-all.sh
@@ -213,7 +213,7 @@ fi
 #change to the inputted (or default) dir
 cd $dir
 
-if [ "$(git rev-parse --is-inside-work-tree)" != "true" ] # if theres no git repofdsf
+if [ "$(git rev-parse --is-inside-work-tree)" != "true" ] # if theres nvdo git repofdsf
 then
 	cd $dir_start
 	echo -e "FATAL ERROR:\n   Not a repo!!\n   Find a repo!!"
